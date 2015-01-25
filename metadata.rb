60d84ef6712358b9027746111385301371334719
Gary Leong <gwleong@gmail.com>
2015-01-24 18:04:37 -0800

    trying to install mariadb galera to simplify things a bit

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,13 @@
-maintainer       "Severalnines AB"
-maintainer_email "support@severalnines.com"
+maintainer       "Gary Leong"
+maintainer_email "gwleong@gmail.com"
 license          "Apache 2.0"
-description      "Installs Galera Cluster for MySQL"
+description      "Installs Galera MariaDB Cluster for MySQL"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.1"
-recipe "server", "Installs Galera Cluster for MySQL"
+version          "0.0.1"
+recipe "server", "Installs Galera MariaDB Cluster for MySQL"
 
 %w{ debian ubuntu centos fedora redhat }.each do |os|
   supports os
 end
+
+depends 'apt', '>= 1.8.2'
---
+++ recipes/install.rb
@@ -0,0 +1,85 @@
+#
+# Cookbook Name:: galera
+# Recipe:: galera_install
+#
+# Copyright 2014, Gary Leong
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+e = execute "apt-get update" do
+  action :nothing
+end
+
+apt_repository "mariadb" do
+  uri "add-apt-repository 'deb http://mirror3.layerjet.com/mariadb/repo/5.5/debian"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "0xcbcb082a1bb943db"
+end
+
+if node['platform'] == "ubuntu"
+  e.run_action(:run)
+end
+
+%w{python-software-properties
+mariadb-server
+}.each do |pkg|
+  package pkg do
+    action :install
+    options "--no-install-recommends"
+  end
+end
+
+#Chef::Log.info "Configuring cluster specified in yml file ..."
+#
+##Making a libary call to read yml file and see if there are any members
+#cluster_ready,cluster_name,members = Chef::ResourceDefinitionList::NodesHelper.cluster_members(node)
+#
+#my_ip = node['ipaddress']
+#
+#if cluster_ready
+#  hosts = []
+#  members.each do |member|
+#    ipaddress = member["ipaddress"]
+#    fqdn = member["fqdn"]
+#    hostname = member["hostname"]
+#    port = member["port"]
+#    hosts << ipaddress
+#  end
+#
+#  node.override['wsrep']['cluster_name'] = cluster_name
+#  #For the init host, we take the first node of the array
+#  init_host = hosts[0]
+#  sync_host = init_host
+#
+#else
+#
+#  Chef::Log.warn "***********************************"
+#  Chef::Log.warn "There are no nodes found for the cluster ..."
+#  #Chef::Log.warn "File exit status set at 185 ..."
+#  Chef::Log.warn "***********************************"
+#
+#  #file "/tmp/file_exit_status" do
+#  #  content "185"
+#  #  owner 'root'
+#  #  group 'root'
+#  #  mode '444'
+#
+#  #end
+#
+#  hosts = nil
+#
+#end
+
