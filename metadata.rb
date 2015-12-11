39e82cfb74df699447d48a5cc3238652fef2812f
root <gwleong@gmail.com>
2015-12-11 16:50:36 +0000

    Automatically committed by jiffy

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "gwleong@gmail.com"
 license          "Apache 2.0"
 description      "Installs Galera MariaDB Cluster for MySQL"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 recipe "server", "Installs Galera MariaDB Cluster for MySQL"
 
 %w{ debian ubuntu centos fedora redhat }.each do |os|
